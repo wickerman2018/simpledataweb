4ac3905197347defc97bbf5db3565fa88ec3f8a9
saltukhov <is1ntc@gmail.com>
2019-12-23 17:03:42 +0200

    Add JPA dependency

---
+++ src/main/java/is/ntc/simpledataweb/Book.java
@@ -0,0 +1,13 @@
+package is.ntc.simpledataweb;
+
+import lombok.Data;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Data
+@Entity
+public class Book {
+
+}
